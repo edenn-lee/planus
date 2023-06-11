8b1523373b014e72c208e98096202499dd633b96
edenn-lee <93193485+edenn-lee@users.noreply.github.com>
2023-06-12 00:51:44 +0900

    진자해치웠나?

--- src/components/EventDetailModal.js
+++ src/components/EventDetailModal.js
@@ -1,66 +1,38 @@
 import { Modal, Button, ModalBody } from 'react-bootstrap';
-import { useState ,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import './EventDetailModal.css';
 import axios from 'axios';
-import EditEventModal from './EditEventModal';
-// import Modal from "react-modal";
 
-const EventDetailModal = ({showEditEvent, show, event, onClose, onDeleteClick, events, setEvents }) => {
+const EventDetailModal = ({ showEditEvent, show, event, onClose, onDeleteClick, events, setEvents }) => {
   const [images, setImages] = useState(event.images);
   const token = localStorage.getItem('token');
   const [comments, setComments] = useState([]);
   const [text, setText] = useState('');
-  const [ScheduleId, setScheduleID] = useState('');
-
-
 
   const handleDeleteClick = () => {
     onDeleteClick(event);
   };
-  const convertBlobToBase64 = (blob) => {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(blob);
-      reader.onload = (event) => resolve(event.target.result);
-      reader.onerror = (error) => reject(error);
-      
-    });
-  };
-  const handleImageUpdate = async (event, imageFile) => {
-  try {
-    const formData = new FormData();
-    formData.append('image', imageFile);
 
-    const response = await axios.patch(
-      `http://13.209.48.48:8080/api/schedules/image/${event.id}`,
-      formData,
-      {
+  const handleImageUpdate = async (event, imageFile) => {
+    try {
+      const formData = new FormData();
+      formData.append('image', imageFile);
+
+      const response = await axios.patch(`http://13.209.48.48:8080/api/schedules/image/${event.id}`, formData, {
         headers: {
           'Authorization': 'Bearer ' + token,
           'Content-Type': 'multipart/form-data',
         },
-      }
-    );
-        console.log(response);
-    const updatedEvent = response.data;
-    const imageResponse = response.data;
-    const arrayBuffer = new Uint8Array.from(atob(imageResponse.data), c => c.charCodeAt(0)).buffer;
-    const byteArray = new Uint8Array(arrayBuffer);
-    const blob = new Blob([byteArray], { type: imageResponse.headers['Content-Type'] });
-    const base64Image = await convertBlobToBase64(blob);
-    setImages(base64Image);
+      });
 
-    console.log(blob);
-    console.log(byteArray);
-    console.log(imageResponse.data);
-    console.log(imageResponse.headers['Content-Type']);
-    console.log(updatedEvent.images);
-    console.log(base64Image);
-    console.log(response.data.images.imageData);
-  } catch (error) {
-    console.error(error);
-  }
-};
+      const updatedEvent = response.data;
+      setImages(updatedEvent.images);
+      const updatedEvents = events.map((evt) => (evt.id === updatedEvent.id ? updatedEvent : evt));
+      setEvents(updatedEvents);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const handleCommentsSubmit = () => {
     axios
@@ -103,30 +75,25 @@ const EventDetailModal = ({showEditEvent, show, event, onClose, onDeleteClick, e
   };
 
   useEffect(() => {
-    handleComments(event);
-  }, [])
-
+    handleComments();
+  }, []);
 
   return (
     <>
-      <Modal show={show}
-      onHide={onClose}
-      className="event-detail-modal"
-      >
+      <Modal show={show} onHide={onClose} className="event-detail-modal">
         <Modal.Header>
           <Modal.Title>{event.title}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {images && <img src={`data:image/jpeg;base64,${images}`} alt="No Images" />}
+          {images && <img src={`data:image/jpeg;base64,${images}`} alt="이미지가업다" />}
           {event.content && <p>{event.content}</p>}
           
           <p>{event.start.toLocaleString()}</p>
           <input type="file" onChange={(e) => handleImageUpdate(event, e.target.files[0])} />
-          
         </Modal.Body>
 
         <Modal.Footer>
-          <Button variant="secondary" onClick={()=>{onClose(); showEditEvent(); }}>
+          <Button variant="secondary" onClick={() => { onClose(); showEditEvent(); }}>
             수정
           </Button>
           <Button variant="danger" onClick={handleDeleteClick}>
@@ -137,61 +104,27 @@ const EventDetailModal = ({showEditEvent, show, event, onClose, onDeleteClick, e
           </Button>
           <div className="comments-section">
             <h3>댓글</h3>
-            <div className="comment">
-              <span className="author">John Doe</span>
-              <p className="content">첫 번째 댓글입니다.</p>
-              <span className="comment-date">2023-06-09 14:30</span>
-            </div>
-            <div className="comment">
-              <span className="author">Jane Smith</span>
-              <p className="content">두 번째 댓글입니다.</p>
-              <span className="comment-date">2023-06-09 15:15</span>
-            </div>
+            {comments.map((comment) => (
+              <div key={comment.id} className="comment">
+                <span className="author">{comment.memberNickname}</span>
+                <p className="content">{comment.text}</p>
+                {/* <span className="comment-date">{comment.date}</span> */}
+              </div>
+            ))}
             <div className="comment-divider"></div>
             <form className="comment-form">
-              <textarea placeholder="댓글을 입력하세요"></textarea>
-              <button type="button" onClick={()=>handleCommentsSubmit(event)}>댓글 작성</button>
+              <textarea
+                placeholder="댓글을 입력하세요"
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+              ></textarea>
+              <button type="button" onClick={handleCommentsSubmit}>
+                댓글 작성
+              </button>
             </form>
-            {/* {groups.map((group) => (
-                  <li style={{ fontSize: '14px' }} key={group.name}>
-                    <label>
-                      <input
-                        type="checkbox"
-                        checked={selectedGroup.some((selected) => selected.id === group.id)}
-                        onChange={(event) => handleGroupCheckboxChange(event, group)}
-                      />
-                      {group.name}
-                    </label>
-
-                    <KakaoShare isButtonDisabled={buttonDisabled}  onShare={{handleShare}}/>
-             
-
-                    {showEditGroup && (
-                      <>
-                        <button className="delete-button" onClick={() => checkDeleteGroupSubmit(group)}>
-                          삭제
-                        </button>
-                      </>
-                    )}
-                  </li>
-                ))} */}
-            
-              {/* {comments.map((comment) => (
-                <div className="comment" key={comment}>
-                  <p className="content">{comment.text}</p>
-                </div>
-              ))}
-               */}
-            
-              
-
-          
           </div>
         </Modal.Footer>
-          
       </Modal>
-
-      
     </>
   );
 };
